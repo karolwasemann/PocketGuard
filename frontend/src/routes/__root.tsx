20060ba4f4ae25e70384b6581548fa0aa921a0e8
Karol Wasemann <kw.kwaq@gmail.com>
2024-05-17 17:00:28 +0200

    chore: Remove TanStackRouterDevtools from root component

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -3,7 +3,6 @@ import {
   Link,
   Outlet,
 } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import { type QueryClient } from '@tanstack/react-query';
 import { Toaster } from '@/components/ui/sonner';
 
@@ -46,7 +45,6 @@ function Root() {
       <hr />
       <Outlet />
       <Toaster />
-      <TanStackRouterDevtools />
     </>
   );
 }
